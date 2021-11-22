1726275b6f3b73c504b0818bed31c1587c181f3c
truongnguyen <truong.nguyen1@onemount.com>
2021-11-22 19:36:44 +0700

    update code

--- Print _Star.py
+++ Print _Star.py
@@ -1,8 +1,19 @@
 print("Print Star")
 int_number1 = input("Nhập vào số dòng: ")
 int_check = int(int_number1)
-star = '*'
+star = ' *'
 space = " "
 for i in range(0,int_check+1):
     print(space*(int_check-i)+star*i)
-
+print("         ")
+for i in range (1,int_check+1):
+    print('  '*(i-1),'* '*(int_check+1-i))
+print("         ")
+for i in range (1,int_check+1):
+    print('  '*(int_check-i),'* '*i)
+print("         ")
+for i in range (1,int_check+1):
+    print('  '*(int_check-i),'* '*(2*i-1),'  '*(int_check-i))
+print("         ")
+for i in range (1,int_check+1):
+    print('* '*int_check)
