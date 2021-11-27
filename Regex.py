5eec924742a3c986d40501cb6fea8f7428779e34
truongnguyen <truong.nguyen1@onemount.com>
2021-11-27 18:43:22 +0700

    update final

--- Regex.py
+++ Regex.py
@@ -1,24 +1,18 @@
 import re
-f = open("place_code.txt", "r") 
-# print(f.read())
-fileJPG = re.compile(r'((\/[a-z\-]+)+\.jpg)')
-matches = re.findall(fileJPG, f.read())
-# print(('List match: ',  [m[0] for m in matches]))
-
-protocol = "http:"
-host = re.compile(r'(\d.\d.\d.\d)')
-checkhost = re.findall(host, f.read())
-print([a[1] for a in checkhost])
-
-url = []
-not_over_lapped = []
-for m in matches:
-	if m[0] not in not_over_lapped:
-		not_over_lapped.append(m[0])
-
-for s in not_over_lapped:
-    newURL = protocol + s
-    url.append(newURL)
-
-print(url)
-# print(not_over_lapped)
+urls = []
+path_pattern = re.compile(r'((\/[a-z\-]+)+\.jpg)')
+host_pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+)')
+with open('place_code.txt', 'r') as f:
+	for line in f.readlines():
+		matches = re.findall(path_pattern, line)
+		if len(matches) >= 1:
+			path = matches[0][0]
+		else:
+			continue
+		hosts = re.findall(host_pattern, line)
+		if len(hosts) >= 2:
+			host = hosts[-1]
+			url = "http://{}{}".format(host, path)
+			if url not in urls:
+				urls.append(url)
+print(urls)	
