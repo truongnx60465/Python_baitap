76ae36d7894cdb5bcd17afbce8efe7e531f9e0ab
truongnguyen <truong.nguyen1@onemount.com>
2021-11-27 13:04:26 +0700

    add xong http , dang tim cach gan host

--- Regex.py
+++ Regex.py
@@ -3,4 +3,22 @@ f = open("place_code.txt", "r")
 # print(f.read())
 fileJPG = re.compile(r'((\/[a-z\-]+)+\.jpg)')
 matches = re.findall(fileJPG, f.read())
-print(('List match: ',  [m[0] for m in matches]))
+# print(('List match: ',  [m[0] for m in matches]))
+
+protocol = "http:"
+host = re.compile(r'(\d.\d.\d.\d)')
+checkhost = re.findall(host, f.read())
+print([a[1] for a in checkhost])
+
+url = []
+not_over_lapped = []
+for m in matches:
+	if m[0] not in not_over_lapped:
+		not_over_lapped.append(m[0])
+
+for s in not_over_lapped:
+    newURL = protocol + s
+    url.append(newURL)
+
+print(url)
+# print(not_over_lapped)
